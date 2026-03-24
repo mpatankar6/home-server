838c4e56f2abb70edd121b7b97433281b0cdaeaa
Mihir Patankar <mpatankar06@gmail.com>
2026-03-24 18:40:33 -0400

    add dns rewrites to adguard home

--- adguardhome.nix
+++ adguardhome.nix
@@ -1,12 +1,24 @@
+let
+  # Assuming there is a static DHCP lease, this should never change.
+  local-ip = "192.168.1.190";
+in
 {
   services.adguardhome = {
     enable = true;
     openFirewall = true;
     settings = {
-      dns.upstream_dns = [
-        "1.1.1.1"
-        "1.0.0.1"
-      ];
+      dns = {
+        upstream_dns = [
+          "1.1.1.1"
+          "1.0.0.1"
+        ];
+        rewrites = [
+          {
+            domain = "adguard.home";
+            answer = local-ip;
+          }
+        ];
+      };
     };
   };
 }
